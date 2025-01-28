c0cb548140fe1007ee619389dad48b889e4a372b
QwertykQwertov <qwertykqwertov@mail.ru>
2025-01-28 16:49:35 +0300

    В pvc_plastic добавлен слайдер (черновой)

--- modules/footer.php
+++ modules/footer.php
@@ -36,6 +36,96 @@
   </div>
 </footer>
 
+
+<!-- Modal -->
+<div class="modal fade" id="modalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header d-flex justify-content-end">
+        <button style="background: transparent; border: none" type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true" class="h4">×</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div id="carouselModal" class="carousel carousel-dark slide">
+          <div class="carousel-inner" id="modalCarouselInner">
+            <div class="carousel-item active">
+              <img class="ki-modal-carousel-item-image" id="modalImage" src="" alt="">
+            </div>
+          </div>
+          <button id="carouselButtonPrev" class="carousel-control-prev" type="button" data-bs-target="#carouselModal" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Предыдущий</span>
+          </button>
+          <button id="carouselButtonNext" class="carousel-control-next" type="button" data-bs-target="#carouselModal" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Следующий</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<script>
+  // Открытие модального окна со слайдером
+  function onImageClick(e) {
+    console.log('wor')
+    if (e.target.classList.contains('ki-modal-trigger')) {
+      console.log('img1')
+      console.dir(e.target.children[0])
+      setSlides(e.target.children[0])
+      // modalImage.src = e.target.children[0].currentSrc
+    } else if (e.target.nodeName === 'IMG') {
+      console.log('img')
+      console.dir(e.target)
+      setSlides(e.target)
+      // modalImage.src = e.target.currentSrc
+    } else {
+      e.stopPropagation()
+    }
+  }
+
+
+  function setSlides(el) {
+    modalCarouselInner.innerHTML = ''
+
+    addSlide(el.src, true)
+
+    let path = el.src.split('/')
+    path.pop()
+    path = path.join('/') + '/'
+
+    const otherImg = el.dataset.images && el.dataset.images.split(',')
+
+    if (!otherImg) {
+      carouselButtonPrev.classList.add('d-none')
+      carouselButtonNext.classList.add('d-none')
+      return
+    } else {
+      carouselButtonPrev.classList.remove('d-none')
+      carouselButtonNext.classList.remove('d-none')
+    }
+
+    otherImg.forEach(imageName => {
+      addSlide(path + imageName.trim())
+    })
+  }
+
+  function addSlide(src, isActive) {
+    const activeItem = document.createElement('div')
+    activeItem.classList.add('carousel-item', 'text-center')
+    if (isActive) activeItem.classList.add('active')
+
+    const img = document.createElement('img')
+    img.classList.add('ki-modal-carousel-item-image')
+    img.src = src
+
+    activeItem.append(img)
+    modalCarouselInner.append(activeItem)
+  }
+</script>
 <script src="/assets/bootstrap/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
   integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
