10f320106415411311673683cf3441f6bb584cfb
QwertykQwertov <qwertykqwertov@mail.ru>
2025-01-29 16:36:13 +0300

    Внедрение фильтра

--- catalog/sandwich/index.php
+++ catalog/sandwich/index.php
@@ -8,7 +8,20 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/modules/header.php";
 
 require($_SERVER['DOCUMENT_ROOT'] . '/database/db.php');
 
-$sql = "SELECT * FROM products_sandwich ORDER BY name";
+var_dump($_GET);
+
+if ($_GET['width'] || $GET['length'] || $GET['thickness']) {
+  $sql = "SELECT * FROM products_sandwich WHERE ";
+  foreach ($_GET as $parameter => $value):
+    // echo ($parameter);
+    $sql = $sql . $parameter . ' = ' . $value . ' AND ';
+  endforeach;
+  echo ($sql);
+} else {
+  $sql = "SELECT * FROM products_sandwich ORDER BY name";
+}
+
+// $sql = "SELECT * FROM products_sandwich ORDER BY name";
 $result = $conn->query($sql);
 
 if ($result->num_rows != 0) {
@@ -18,6 +31,40 @@ if ($result->num_rows != 0) {
 ?>
 
 <div class="container my-5">
+  <div class="row row-cols-1 row-cols-md-4 g-4">
+    <div class="col">
+      <label for="width">Ширина</label>
+      <select id="width" class="form-control">
+        <option value="null" selected>Не выбрано</option>
+        <option value="3000">3000</option>
+      </select>
+    </div>
+    <div class="col">
+      <label for="length1">Длина</label>
+      <select id="length1" class="form-control">
+        <option value="null" selected>Не выбрано</option>
+        <option value="1500">1500</option>
+        <option value="2000">2000</option>
+      </select>
+    </div>
+    <div class="col">
+      <label for="thickness">Толщина</label>
+      <select id="thickness" class="form-control">
+        <option value="null" selected>Не выбрано</option>
+        <option value="10">10 мм</option>
+        <option value="24">24 мм</option>
+        <option value="32">32 мм</option>
+        <option value="36">36 мм</option>
+        <option value="40">40 мм</option>
+        <option value="44">44 мм</option>
+        <option value="46">46 мм</option>
+      </select>
+    </div>
+    <div class="col d-flex align-items-end">
+      <button class="btn btn-dark" onclick="setFilter()">Применить</button>
+    </div>
+  </div>
+
   <div class="container mt-5 mb-5">
     <div class="row row-cols-1 row-cols-md-4 g-4 justify-content-center">
       <?php foreach ($products as $product): ?>
@@ -47,3 +94,26 @@ if ($result->num_rows != 0) {
 </div>
 
 <? include_once $_SERVER['DOCUMENT_ROOT'] . "/modules/footer.php"; ?>
+
+<script>
+  function setFilter() {
+    let filterString = ''
+
+    const filterValues = {
+      width: width.value,
+      length: length1.value,
+      thickness: thickness.value
+    }
+
+    for (let key in filterValues) {
+      if (filterValues[key] != 'null') {
+        filterString += `${key}=${filterValues[key]}&`
+      }
+    }
+
+    if (filterString) {
+      window.location.search = filterString.slice(0, -1)
+    }
+
+  }
+</script>
